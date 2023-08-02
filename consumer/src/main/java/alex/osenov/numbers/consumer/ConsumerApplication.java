82a1c013b98a43ce206e35c91df7def6b1dd8976
Aleksandar Osenov <aosenov@gmail.com>
2023-08-02 20:32:33 +0300

    Adding apps

---
+++ consumer/src/main/java/alex/osenov/numbers/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package alex.osenov.numbers.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
